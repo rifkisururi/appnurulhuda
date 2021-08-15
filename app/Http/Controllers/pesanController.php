98c13e8c8762cc8a8f149c9663595d7bb76ffd57
Rifki Ahmad Sururi <rifkisururi27@gmail.com>
2021-08-15 18:48:18 +0700

    update bos

--- app/Http/Controllers/pesanController.php
+++ app/Http/Controllers/pesanController.php
@@ -40,7 +40,7 @@ class pesanController extends Controller
             $type = 1;
         } else if ($media == "whatsapp") {
             $sender = env("SENDER_WA");
-            $isiPesan = $s = str_replace("/n", '<br>', $isiPesan);
+            $isiPesan = str_replace("/n", '<br>', $isiPesan);
             $type = 0;
         }
 
